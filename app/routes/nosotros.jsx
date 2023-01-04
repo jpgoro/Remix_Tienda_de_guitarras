db8a26148006a6e9adbdab4d8cf112486e688554
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-01-04 18:14:58 -0300

    Creando y estilando seccion nosotros

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,7 +1,38 @@
+import imagen from "../../public/img/nosotros.jpg"
+import styles from '~/styles/nosotros.css';
+
+export function meta(){
+  return {
+    title:'GuitarLA - Sobre Nosotros',
+    description:'Venta de guitarras, blog de música'
+  }
+}
+export function Links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image'
+    }
+  ]
+}
 
 function Nosotros() {
   return (
-    <div>nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+    <div className="contenido">
+      <img src={imagen} alt="Imagen sobre nosotros" />
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non quam eleifend, mollis ipsum id, porttitor dolor. Mauris facilisis tempor risus vitae convallis. Etiam justo tortor, vestibulum ut elit sit amet, ultricies placerat nunc. Aenean et tempus eros. Suspendisse et sollicitudin augue. Sed pretium lectus sed lobortis condimentum. Ut ut vehicula orci, sed tincidunt diam.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non quam eleifend, mollis ipsum id, porttitor dolor. Mauris facilisis tempor risus vitae convallis. Etiam justo tortor, vestibulum ut elit sit amet, ultricies placerat nunc. Aenean et tempus eros. Suspendisse et sollicitudin augue. Sed pretium lectus sed lobortis condimentum. Ut ut vehicula orci, sed tincidunt diam.</p>
+      </div>
+    </div>
+    </main>
   )
 }
 
