f29c6775ed9f717ba0d5d43950f5fa83e66780d3
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-01-06 16:22:45 -0300

    Creando componente de blogs

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,7 +1,28 @@
+import { useLoaderData } from "@remix-run/react"
+import Post from "~/components/post"
+import { getPosts } from "~/models/posts.server"
+
+export async function loader(){
+const posts = await getPosts()
+return (posts.data)
+}
+
 
 function Blog() {
+  const posts = useLoaderData()
+
   return (
-    <div>blog</div>
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {posts.map(post => (
+          <Post
+          key={post.id}
+          post={post.attributes}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
