ffdf163c7329b0972569388043b2b5639db627b5
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-01-05 21:32:15 -0300

    Generando router dinamico para las guitarras

---
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -0,0 +1,44 @@
+import { useLoaderData } from "@remix-run/react"
+import {getGuitarra} from "~/models/guitarras.sever"
+import styles from "~/styles/guitarras.css"
+
+export function meta({data}){
+  return {
+    title: `GuitarLA - ${data.data[0].attributes.nombre}`,
+    description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}`
+
+  }
+}
+
+export function links(){
+ return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
+
+
+export async function loader({params}){
+   const {guitarraUrl} = params
+   const guitarra1 = await getGuitarra(guitarraUrl)
+   return guitarra1
+}
+
+function Guitarra() {
+  const guitarra = useLoaderData();
+  const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
+  return (
+    <main className="contenedor guitarra">
+      <img className="imagen" src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
+  )
+}
+
+export default Guitarra
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,7 +1,45 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.sever"
+import Guitarra from "~/components/guitarra";
+import styles from "~/styles/guitarras.css"
+
+export function meta(){
+  return {
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'GutiarLA - Nuestra colección de guitarras'
+  }
+}
+
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
+
+export async function loader(){
+  const guitarras = await getGuitarras()
+  return guitarras.data
+}
 
 function Tienda() {
+  const guitarras = useLoaderData();
   return (
-    <div>tienda</div>
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Colección</h2>
+      {guitarras.length && (
+        <div className="guitarras-grid">
+          {guitarras.map( guitarra => (
+            <Guitarra
+              key = {guitarra.id}
+              guitarra={guitarra}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
