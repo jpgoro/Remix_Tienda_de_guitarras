9491893c823f87896eb5a09545065cdd65721036
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-01-07 17:06:33 -0300

    Estilos a los componentes blog y post

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -1,3 +1,7 @@
+/**
+ * Obtiene las publicaciones de la API y las devuelve como un objeto JSON
+ * @returns Una matriz de objetos.
+ */
 export async function getPosts(){
     const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
     return await respuesta.json();
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,29 +1,35 @@
-import { useLoaderData } from "@remix-run/react"
-import Post from "~/components/post"
-import { getPosts } from "~/models/posts.server"
+import { useLoaderData } from "@remix-run/react";
+import Post from "~/components/post";
+import { getPosts } from "~/models/posts.server";
+import styles from "~/styles/blog.css";
 
-export async function loader(){
-const posts = await getPosts()
-return (posts.data)
+export async function loader() {
+  const posts = await getPosts();
+  return posts.data;
 }
 
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 function Blog() {
-  const posts = useLoaderData()
+  const posts = useLoaderData();
 
   return (
     <main className="contenedor">
       <h2 className="heading">Blog</h2>
       <div className="blog">
-        {posts.map(post => (
-          <Post
-          key={post.id}
-          post={post.attributes}
-          />
+        {posts.map((post) => (
+          <Post key={post.id} post={post.attributes} />
         ))}
       </div>
     </main>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
