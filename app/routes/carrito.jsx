c273f8c349b812c7d2848dae6b1967d8672cd011
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-01-11 21:37:46 -0300

    Agredado de carrito de compras

---
+++ app/routes/carrito.jsx
@@ -0,0 +1,35 @@
+import styles from "~/styles/carrito.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+export function meta() {
+  return{
+    title: "GuitarLA - Carrito de Compras",
+    description: "Venta de guitarras, música, blog, carrito de compras, tienda"
+  }
+}
+
+function Carrito() {
+  return (
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Articulos</h2>
+        </div>
+        <aside className="resumen">
+          <h3>Resumen del pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </div>
+    </main>
+  );
+}
+
+export default Carrito;
